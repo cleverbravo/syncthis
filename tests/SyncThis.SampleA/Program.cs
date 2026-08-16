2a4838a4ef0d56afaaf2e223c2a854f7183a4361
clever bravo <cleverbravov@gmail.com>
2026-08-15 21:43:44 -0400

    simple broadcast sample added

---
+++ tests/SyncThis.SampleA/Program.cs
@@ -0,0 +1,22 @@
+﻿using SyncThis.Core;
+using SyncThis.Sync;
+
+Console.WriteLine("Starting A");
+
+var hostA = new Sync(new SyncEngine(SyncConfigFactory.Broadcast(42400)));
+var data = new Data { X = 30 };
+hostA.SyncThis(data);
+Console.WriteLine("  HostA sent initial snapshot (X=30)");
+
+do
+{
+    Console.Write($"Enter the new value of int X(0 ends the program)=");
+}while((data.X=int.Parse(Console.ReadLine()))!=0);//possible exception but is just a sample
+
+hostA.Stop();
+
+
+class Data:Syncable
+{
+    public int X { get; set; }
+}
---
+++ tests/SyncThis.SampleB/Program.cs
@@ -0,0 +1,20 @@
+﻿using SyncThis.Core;
+using SyncThis.Sync;
+
+Console.WriteLine("Starting B");
+var hostB = new Sync(new SyncEngine(SyncConfigFactory.Broadcast(42400)));
+
+hostB.OnUpdate<Data>(p => { 
+    Console.WriteLine($"new value for p.X={p.X}");
+});
+
+Console.ReadKey();
+
+return 0;
+
+
+class Data:Syncable
+{
+    public int X { get; set; }
+}
+
